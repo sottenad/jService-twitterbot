c246c8f3d70fec5cfbdde237c85bada84348ff1f
Steve Ottenad <sottenad@gmail.com>
2015-02-01 13:15:12 -0800

    removing local config

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    twitter_consumer_key:         'aK7E4Xgx2nmF1uP771DSDXHeI',
-    twitter_consumer_secret:      'GGpXQzRwaKkf8sVQaZReA2OEJE5eRw5XzFY8Wp8nnWXS499klt',
-    twitter_access_token:         '2991209577-10QtmeiK8qQjszkUOf9CvGbt8Fk9NLQqIEHklKg',
-    twitter_access_token_secret:  'wIAKj7a2GUuvniGK07Dp4Ym4XPe6zJBNtMBAQ4Q1kVPxe',
-    bitly_id: '',
-    bitly_secret: '',
-}
- 
